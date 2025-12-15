11cd12b8316a6dcbde1edfb36740f537851d0a5f
Bich Ngoc <bichngoc@gmail.com>
2025-12-15 09:40:45 +0700

    update

--- BTVN_nhóm2/cypress/e2e/baitap15.cy.js
+++ BTVN_nhóm2/cypress/e2e/baitap15.cy.js
@@ -13,71 +13,32 @@ describe("TB_15 - Update Contact Details", () => {
   };
 
   it("User can update contact information successfully", () => {
-    // 1. Login
-    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    // Login
+    cy.loginOrangeHRM("Admin", "admin123");
 
-    cy.get('[name="username"]').type("Admin");
-    cy.get('[name="password"]').type("admin123");
-    cy.get('button[type="submit"]').click();
-
-    // Verify login success
-    cy.url().should("include", "/dashboard");
-
-    // 2. Vào My Info
+    // My Info → Contact Details
     cy.contains("My Info").click();
-
-    // 3. Chuyển tab Contact Details
     cy.contains("Contact Details").click();
 
-    // 4. Hàm điền input theo label
-    const fillInput = (labelText, value) => {
-      cy.contains("label", labelText)
-        .parents(".oxd-input-group")
-        .find("input")
-        .clear()
-        .type(value);
-    };
+    // Fill form
+    cy.fillInputByLabel("Street 1", contactData.street1);
+    cy.fillInputByLabel("Street 2", contactData.street2);
+    cy.fillInputByLabel("City", contactData.city);
+    cy.fillInputByLabel("State/Province", contactData.state);
+    cy.fillInputByLabel("Zip/Postal Code", contactData.zip);
+    cy.fillInputByLabel("Mobile", contactData.mobile);
+    cy.fillInputByLabel("Work", contactData.work);
+    cy.fillInputByLabel("Work Email", contactData.workEmail);
+    cy.fillInputByLabel("Other Email", contactData.otherEmail);
 
-    fillInput("Street 1", contactData.street1);
-    fillInput("Street 2", contactData.street2);
-    fillInput("City", contactData.city);
-    fillInput("State/Province", contactData.state);
-    fillInput("Zip/Postal Code", contactData.zip);
-    fillInput("Mobile", contactData.mobile);
-    fillInput("Work", contactData.work);
-    fillInput("Work Email", contactData.workEmail);
-    fillInput("Other Email", contactData.otherEmail);
+    cy.selectDropdownByLabel("Country", contactData.country);
 
-    // 5. Chọn Country (dropdown custom của OrangeHRM)
-    cy.contains("label", "Country")
-      .parents(".oxd-input-group")
-      .find(".oxd-select-text")
-      .click();
-
-    cy.get(".oxd-select-dropdown")
-      .contains(".oxd-select-option", contactData.country)
-      .click();
-
-    // 6. Save
+    // Save
     cy.contains("button", "Save").click();
 
-    // 7. Verify message Successfully Updated
+    // Verify
     cy.contains("Successfully Updated").should("be.visible");
 
-    // 8. Verify dữ liệu
-    cy.contains("label", "Street 1")
-      .parents(".oxd-input-group")
-      .find("input")
-      .should("have.value", contactData.street1);
-
-    cy.contains("label", "City")
-      .parents(".oxd-input-group")
-      .find("input")
-      .should("have.value", contactData.city);
-
-    cy.contains("label", "Mobile")
-      .parents(".oxd-input-group")
-      .find("input")
-      .should("have.value", contactData.mobile);
+    
   });
 });
--- BTVN_nhóm2/cypress/support/commands.js
+++ BTVN_nhóm2/cypress/support/commands.js
@@ -1,25 +1,26 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("fillInputByLabel", (labelText, value) => {
+  cy.contains("label", labelText)
+    .parents(".oxd-input-group")
+    .find("input")
+    .clear()
+    .type(value);
+});
+Cypress.Commands.add("selectDropdownByLabel", (labelText, optionText) => {
+  cy.contains("label", labelText)
+    .parents(".oxd-input-group")
+    .find(".oxd-select-text")
+    .click();
+
+  cy.get(".oxd-select-dropdown")
+    .contains(".oxd-select-option", optionText)
+    .click();
+});
+Cypress.Commands.add("loginOrangeHRM", (username, password) => {
+  cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+  cy.get('[name="username"]').type(username);
+  cy.get('[name="password"]').type(password);
+  cy.get('button[type="submit"]').click();
+
+  cy.url().should("include", "/dashboard");
+});
