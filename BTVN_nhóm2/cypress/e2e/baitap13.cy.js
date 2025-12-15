32e2c9354db0d7da28f1f074e5d5453516b09bc0
Ngthuthuy7997 <nthuthuy99999@gmail.com>
2025-12-15 11:04:59 +0700

    save local changes before merge

--- BTVN_nhóm2/cypress/e2e/baitap13.cy.js
+++ BTVN_nhóm2/cypress/e2e/baitap13.cy.js
@@ -6,25 +6,25 @@ describe("TB_13 - Kiểm tra và xử lý text trong iframe", () => {
     cy.visit("https://the-internet.herokuapp.com/iframe");
   });
 
-  
+
   it("Xóa text cũ và nhập 'Hello Cypress'", () => {
     cy.get('#mce_0_ifr').then($iframe => {
-  const body = $iframe.contents().find('body');
+      const body = $iframe.contents().find('body');
 
-  // Set body là editable nếu chưa có
-  cy.wrap(body)
-    .invoke("attr", "contenteditable", "true")
-    .invoke("removeClass", "mce-content-readonly");
+      // Set body là editable nếu chưa có
+      cy.wrap(body)
+        .invoke("attr", "contenteditable", "true")
+        .invoke("removeClass", "mce-content-readonly");
 
-  // Truy cập element typeable trong body và type
-  cy.wrap(body)
-    .find('p')         // TinyMCE mặc định có <p>
-    .clear()
-    .type("Hello Cypress");
-  
-  // Assert
-  cy.wrap(body).should('contain.text', 'Hello Cypress');
-});
+      // Truy cập element typeable trong body và type
+      cy.wrap(body)
+        .find('p')         // TinyMCE mặc định có <p>
+        .clear()
+        .type("Hello Cypress");
+
+      // Assert
+      cy.wrap(body).should('contain.text', 'Hello Cypress');
+    });
 
 
   });
