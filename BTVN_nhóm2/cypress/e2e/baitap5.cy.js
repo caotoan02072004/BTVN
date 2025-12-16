43f6a8d280a16da49ba0521a597dd88d62db3324
Bich Ngoc <bichngoc@gmail.com>
2025-12-16 11:44:07 +0700

    Update

--- BTVN_nhóm2/cypress/e2e/baitap5.cy.js
+++ BTVN_nhóm2/cypress/e2e/baitap5.cy.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 describe("CB_5 - Tìm kiếm sản phẩm", () => {
   it('CB_5 - Tìm kiếm sản phẩm ', () => {
-      cy.visit('https://demowebshop.tricentis.com/');
-      cy.get('#small-searchterms').type('computer');
-      cy.get('input[value="Search"]').click();
-      cy.get('.product-item').should('exist');
-   });v
-    });
-  
+    cy.visit('https://demowebshop.tricentis.com/');
+    cy.get('#small-searchterms').type('computer');
+    cy.get('input[value="Search"]').click();
+    cy.get('.product-item').should('exist');
+  }); 
+});
+
 
 
